997832ef1fce3ba0e388efcb4f881d9df2f5dc46
Wiktor <wiktorkalinowski@gmail.com>
2022-06-21 11:18:42 +0200

    Added jUnit test for useCases

---
+++ app/src/test/java/com/example/supertajnyprojekt/features/characters/domain/GetLocationUseCaseTest.kt
@@ -0,0 +1,28 @@
+package com.example.supertajnyprojekt.features.characters.domain
+
+import com.example.supertajnyprojekt.features.characters.presentation.LocationRepository
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import org.junit.jupiter.api.Test
+
+internal class GetLocationUseCaseTest {
+
+    @OptIn(DelicateCoroutinesApi::class)
+    @Test
+    fun `when use case is invoked than execute getCharacters method from repository`() {
+        // given
+        val repository = mockk<LocationRepository>(relaxed = true)
+        val useCase = GetLocationUseCase(repository)
+
+        //when
+        useCase(
+            params = Unit,
+            scope = GlobalScope
+        )
+
+        //then
+        coVerify { repository.getLocations() }
+    }
+}
