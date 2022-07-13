5bc10c03774ed9d7d84b146a4f775f969987ffa7
Wiktor <wiktorkalinowski@gmail.com>
2022-07-13 12:32:09 +0200

    Added DI for error wrapper & error mapper

--- app/src/main/java/com/example/supertajnyprojekt/core/di/AppModule.kt
+++ app/src/main/java/com/example/supertajnyprojekt/core/di/AppModule.kt
@@ -5,6 +5,10 @@ import android.net.ConnectivityManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.supertajnyprojekt.core.exception.ErrorMapper
+import com.example.supertajnyprojekt.core.exception.ErrorMapperImpl
+import com.example.supertajnyprojekt.core.exception.ErrorWrapper
+import com.example.supertajnyprojekt.core.exception.ErrorWrapperImpl
 import com.example.supertajnyprojekt.core.network.NetworkStateProviderImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -15,4 +19,6 @@ val appModule = module {
     factory { DividerItemDecoration(androidContext(), LinearLayoutManager.VERTICAL) }
     factory { androidContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
     factory { NetworkStateProviderImpl(get()) }
+    factory<ErrorWrapper> { ErrorWrapperImpl() }
+    factory<ErrorMapper> { ErrorMapperImpl(androidContext()) }
 }
